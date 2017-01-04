326c956ce01ddf8cc9c6143070d635f905740b26
Merve <m.merve.unlu@gmail.com>
2017-01-04 21:39:17 +0200

    starting chunking, added grammars

---
+++ NamedEntityRecognition/NERChunking.py
@@ -0,0 +1,7 @@
+# Grammars
+# MIR-PHRASE = {<MIR|MIR-SHORT><NN>*} 
+# CANCER-PHRASE =  {<DT>*<JJ>*<CANCER><NN>*}
+# EXP-N-PHRASE = {<DT>*<JJ>*<EXP-N><IN>?}
+# EXP-PASSIVE-PHRASE = {<EXP-V>}
+# EXP-ADJ-PHRASE = {<><EXP-ADJ><NN>}
+
