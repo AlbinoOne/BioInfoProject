9801c89404a109b090d75a64b4e14d79029a9240
Merve <m.merve.unlu@gmail.com>
2017-01-04 22:26:40 +0200

    chunk grammars are added

--- NamedEntityRecognition/NERChunking.py
+++ NamedEntityRecognition/NERChunking.py
@@ -1,7 +1,13 @@
-# Grammars
-# MIR-PHRASE = {<MIR|MIR-SHORT><NN>*} 
-# CANCER-PHRASE =  {<DT>*<JJ>*<CANCER><NN>*}
-# EXP-N-PHRASE = {<DT>*<JJ>*<EXP-N><IN>?}
-# EXP-PASSIVE-PHRASE = {<EXP-V>}
-# EXP-ADJ-PHRASE = {<><EXP-ADJ><NN>}
+import nltk
 
+def chunking_sentences(sentences):
+    grammar = r"""
+        MIR-PHRASE :{<MIR|MIR-SHORT><NN>*} 
+        CANCER-PHRASE :{<DT>*<JJ>*<CANCER><NN>*}
+        EXP-N-PHRASE:{<DT>*<JJ>*<EXP-N><IN>?}
+        EXP-PASSIVE-PHRASE:{<EXP-PASTV>}
+        EXP-ADJ-PHRASE:{<DT>*<NN>*<EXP-ADJ><NN>}
+    """     
+    cp = nltk.RegexpParser(grammar)
+    sent_tree = cp.parse_sents(sentences)   
+    return sent_tree
