9da34b42b89edb5ac18b8695963fd7b2267332db
Merve <m.merve.unlu@gmail.com>
2017-01-05 11:08:05 +0200

    grammars added

--- NamedEntityRecognition/NERChunking.py
+++ NamedEntityRecognition/NERChunking.py
@@ -1,13 +1,20 @@
 import nltk
 
+# Tier1 = MIR-PHRASE + EXP-PHRASE + CANCER-PHRASE 
+# Tier2 = MIR-PHRASE + 'as' + EXP-N-PHRASE + 'of' + CANCER-PHRASE 
+# Tier2 = EXP-ADJ + MIR-PHRASE + 'in' + CANCER-PHRASE
+#   (<DT>*<EXP-N><IN>) (<MIR><VBZ><DT>*<JJ>*<TO>) (<EXP-V>) (<CANCER>)
+
 def chunking_sentences(sentences):
     grammar = r"""
         MIR-PHRASE :{<MIR|MIR-SHORT><NN>*} 
-        CANCER-PHRASE :{<DT>*<JJ>*<CANCER><NN>*}
+        CANCER-PHRASE :{<DT>*<JJ>*<CANCER|CANCER-N><NN|NNS>*}
         EXP-N-PHRASE:{<DT>*<JJ>*<EXP-N><IN>?}
         EXP-PASSIVE-PHRASE:{<EXP-PASTV>}
         EXP-ADJ-PHRASE:{<DT>*<NN>*<EXP-ADJ><NN>}
     """     
     cp = nltk.RegexpParser(grammar)
     sent_tree = cp.parse_sents(sentences)   
+    
     return sent_tree
+    
